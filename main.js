bfee38e3601b71ce679e181effd4313bc7ad5006
Julia Banerjee <juliabanerjee@Julias-MacBook-Pro.local>
2021-11-29 17:01:55 +0000

    linked tobuttons on contact page except CV

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 // getting burger menu working for mobile
 
 const menuIcon = document.querySelector("#menu-icon")
-const menuItems = document.querySelector(".burgermenu__items")
+const closeMenuIcon = document.querySelector("#close-menu-icon")
+//const menuItems = document.querySelector(".burgermenu__items")
 
-menuIcon.addEventListener
+
+menuIcon.addEventListener (click, (event =>{
+
+}))
+
+const menuItems =
+`<div class="burgermenu__items">
+  <a href="home.html">Home</a>
+  <a href="about.html">About
+  </a>
+  <a href="experience.html">Experience</a>
+  <a href="work.html">Work</a>
+  <a href="contact.html">Contact</a>
+</div>`
