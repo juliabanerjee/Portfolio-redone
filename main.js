d3b7913c8766f0d768c41928aaec2151a6983f2d
Julia Banerjee <juliabanerjee@Julias-MacBook-Pro.local>
2021-11-29 15:40:43 +0000

    starting burger menu

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 // getting burger menu working for mobile
 
 const menuIcon = document.querySelector("#menu-icon")
+const menuItems = document.querySelector(".burgermenu__items")
 
+menuIcon.addEventListener
